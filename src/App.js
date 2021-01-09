90374063b44b765a21278e6b439ac8173b235add
Nicholas McMaster <nmcmaster@gmail.com>
2021-01-09 16:36:43 -0500

    test commit after repo change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className="App">
-      <p className="bg-blue-100">news app hello world</p>
-      <p className="bg-up-green">news app hello world</p>
+      <p className="bg-blue-100">nasa app hello world</p>
+      <p className="bg-up-green">nasa app test</p>
     </div>
   );
 }
